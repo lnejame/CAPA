9ce238c565f300d6637fb471a09de893afe55f51
Larry NeJame <flohtpc@gmail.com>
2013-12-29 20:14:17 -0500

    new CAPA app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CAPA::Application.config.secret_key_base = 'e630e539bb5e3863bd7457a603db3af9bbc105a60fbbea2f574903de2cc7495e7503438b090363fb97882ed34cb78ed7b26f40dd6b6ed24746b47a2da23d7c29'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CAPA::Application.config.session_store :cookie_store, key: '_CAPA_session'
